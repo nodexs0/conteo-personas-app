36c7157e0918d2bcc86971d8045f60133523801a
ay1309 <ann_yoto@naver.com>
2025-10-20 23:03:12 -0600

    ajustesPantalla

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 // screens/HomeScreen.js
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
@@ -9,13 +9,8 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bienvenido a la App de Conteo de Personas</Text>
-      <Text style={styles.subtitle}> Pulsa el botón para iniciar el conteo.
+      <Text style={styles.subtitle}> Diríjase a la ventana de cámara para comenzar el conteo.
       </Text>
-      <Button
-        title="Iniciar Conteo"
-        onPress={() => navigation.navigate("Cámara")}
-        color="#1E90FF"
-      />
     </View>
   );
 }
