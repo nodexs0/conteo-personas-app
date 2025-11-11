f76a9ed957bb89eff49d0c47abcade19cf1b9793
ay1309 <ann_yoto@naver.com>
2025-11-10 19:38:05 -0600

    correcionLoginAndroid

--- AuthContext.js
+++ AuthContext.js
@@ -2,29 +2,30 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform } from 'react-native';
 
 WebBrowser.maybeCompleteAuthSession();
-
 
 export const AuthContext = createContext();
 
 
-const WEB_CLIENT_ID = '71881995759-ehtfvr76kl4lad72gdhbf0tclg1ikuj6.apps.googleusercontent.com'; 
+const CLIENT_ID = '71881995759-ehtfvr76kl4lad72gdhbf0tclg1ikuj6.apps.googleusercontent.com'; 
 
 const USER_STORAGE_KEY = 'user_data_expo_app';
-
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-
   const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: WEB_CLIENT_ID,
+    webClientId: CLIENT_ID,
+
+    androidClientId: CLIENT_ID, 
     scopes: ['profile', 'email'],
+
+    useProxy: true, 
   });
 
+ 
   useEffect(() => {
     if (response?.type === 'success') {
       const { authentication } = response;
@@ -32,7 +33,7 @@ export const AuthProvider = ({ children }) => {
     }
   }, [response]);
 
-
+  
   const fetchUserInfo = async (token) => {
     try {
       const infoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
@@ -67,6 +68,7 @@ export const AuthProvider = ({ children }) => {
     loadUser();
   }, []);
 
+
   const signIn = async () => {
     try {
       if (!request) {
@@ -79,7 +81,7 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-
+  // Función para cerrar sesión
   const signOut = async () => {
     try {
       await AsyncStorage.removeItem(USER_STORAGE_KEY);
@@ -89,7 +91,6 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-
   return (
     <AuthContext.Provider value={{ user, signIn, signOut, isLoading }}>
       {children}
@@ -97,5 +98,4 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-
-export const useAuth = () => useContext(AuthContext); 
+export const useAuth = () => useContext(AuthContext);
